ad2ed510303dce280bd322db3cbbf554a5a1fd4c
colin <satherc@outlook.com>
2021-07-19 01:40:36 -0400

    added testing data and more  shell functions

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ subtitle = "Version 1.0.0\n"
 print(title)                                                                                      
 print(subtitle)
 
-# TODO: use better practices here such as OOP.
-main_master_file = ""
-main_output_file = ""
-
 
 # FIND CATEGORIES FOR A TRANSACTION DESCRIPTION [WIP].
 #def find_categories(df):
@@ -48,61 +44,55 @@ main_output_file = ""
 #    logf = open(error_log, "w")
 #
 
-def merge_master_file(master_file, in_file):
-    # Append selected file to the master file in a new dataframe
-    a = pd.read_csv(master_file, index_col=False)
-    b = pd.read_csv(in_file, index_col=False)
 
-    len_row = len(b.loc[0])
-    b.drop([0, len_row])
-    out = a.append(b, ignore_index=False)
-
-    formatter = ExcelFormat()
-    formatter.format_cols(out)
-
-    # TODO: use supervised learning libs to match categories (optional).
-    #find_categories(b)
-
-    with open(master_file, 'a', encoding='utf-8', newline = '\n') as f:
-        out.to_csv(f, index=False)
-        f.close()
-    
-    # TODO: Log merge in config file's history [WIP]
-    #with open(config_file_path, 'a') as hist:
-    #    data = in_file+","
-    #    hist.write(data)
-    #    hist.close()
-
-
-def run_merge():
-    try:
-        cwd = os.path.abspath(os.path.dirname(__file__))
-        given_path = "./config/config.ini"
-        config_file_path = os.path.abspath(os.path.join(cwd, given_path))
-
-        config = ConfigParser()
-        config.read(config_file_path)
-        master_file = config["PATHS"]["MasterFile"]
-        output_file = config["PATHS"]["OutputFile"]
-        
-        # Prepare the master file to be appended by the selected csv
-        main_master_file = master_file
-        main_output_file = output_file
-        in_file = sys.argv[1]
-
-        merge_master_file(main_master_file, in_file)
-
-    except:
-        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), "excelerator was not able to import the selected csv to the master file.")
-        #logf.write(str(e)+"\n")
-        return False
+#def run_merge():
+#    try:
+#        cwd = os.path.abspath(os.path.dirname(__file__))
+#        given_path = "./config/config.ini"
+#        config_file_path = os.path.abspath(os.path.join(cwd, given_path))
+#
+#        config = ConfigParser()
+#        config.read(config_file_path)
+#        master_file = config["PATHS"]["MasterFile"]
+#        output_file = config["PATHS"]["OutputFile"]
+#        
+#        # Prepare the master file to be appended by the selected csv
+#        main_master_file = master_file
+#        main_output_file = output_file
+#        in_file = sys.argv[1]
+#
+#        append_to_master(main_master_file, in_file)
+#
+#    except:
+#        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), "excelerator was not able to import the selected csv to the master file.")
+#        #logf.write(str(e)+"\n")
+#        return False
 
 
 class CSV_Controller:
     def date_sort(self, file_path):
-        data = csv.reader(open(file_path,'r'))
-        data = sorted(data, key = lambda row: datetime.strptime(row[0], "%d-%b-%y"))
+        data = pd.read_csv(file_path)
+        data.sort_values(by = 'Date', ascending = False, inplace = True)
         return data
+
+    def add_to_main(self, main_file, in_file):
+        # Append selected file to the master file in a new dataframe
+        a = pd.read_csv(main_file, index_col=False)
+        b = pd.read_csv(in_file, index_col=False)
+
+        len_row = len(b.loc[0])
+        b.drop([0, len_row])
+        out = a.append(b, ignore_index=False)
+
+        #formatter = ExcelFormat()
+        #formatter.format_cols(out)
+
+        # TODO: use supervised learning libs to match categories (optional).
+        #find_categories(b)
+
+        with open(main_file, 'a', encoding='utf-8', newline = '\n') as f:
+            out.to_csv(f, index=False)
+            f.close()
 
 
 # MAIN EXECUTION SECTION
@@ -133,21 +123,31 @@ if __name__ == "__main__":
     config.read(config_file_path)
     flag = True
 
+    cc = CSV_Controller()
+    export_dir = config._sections["Exports"]["path"]
+
     while flag:
         x = input("E-Shell> ")
         if x == "accounts" or x == "show accounts":
             print(config._sections["Accounts"])
+
         elif x == "sort":
             # temp function
-            cc = CSV_Controller()
-            tmp = cc.date_sort("temp_input/test.csv")
+            exports = os.listdir(export_dir)
+            tmp = cc.date_sort(export_dir+exports[0])
             print(tmp)
+
+        elif x == "add":
+            main = config._sections["Accounts"]["test1"]
+            in_file = config._sections["Accounts"]["test2"]
+            cc.add_to_main(main, in_file)
+
         elif x == "exports" or x == "show exports":
-            e = config._sections["Exports"]["path"]
             counter = 1
-            for file in os.listdir(e):
+            for file in os.listdir(export_dir):
                 print("[{}]".format(counter), file)
                 counter += 1
+
         elif x == "exit":
             print("Bye.")
             flag = False
